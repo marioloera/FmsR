bf286dd103d677ba6d62bf4069cb20a63d6624a4
Mario Loera <marioll@kth.se>
2020-09-09 19:35:26 +0200

    start tutorial 1

---
+++ tutorial_1.R
@@ -0,0 +1,9 @@
+# clean workspace and variables
+rm(list = setdiff(ls(), lsf.str()))
+
+# set working directory
+setwd("C:/Users/MarioLoeraLozano/Dropbox/R/")
+
+# loading data
+load("2011-08-ABB.ST")
+
