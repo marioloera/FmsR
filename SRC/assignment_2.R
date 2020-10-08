7141d232bb446a7317fafd96ee75990cbd2dafba
Mario Loera <marioll@kth.se>
2020-10-08 13:47:03 +0200

    assignment 2 prepare data

--- SRC/assignment_2.R
+++ SRC/assignment_2.R
@@ -1,5 +1,5 @@
 # set working directory
-setwd("C:/Users/MarioLoeraLozano/Dropbox/R/")
+setwd("C:/Users/MarioLoeraLozano/Dropbox/R/Data")
 
 # packages needed
 library(plyr) # install.packages('plyr')
@@ -9,7 +9,35 @@ library(zoo) # install.packages('zoo')
 rm(list = setdiff(ls(), lsf.str()))
 
 # loading data
-load("Data/SKFb_Mar2020")
+load("SKFb_Mar2020")
 # tq: 810224 obs. of 9 variables
 
-table(tq$Date, tq$Type)
+# create a variable for time
+time = strptime(tq$Time, format="%H:%M:%OS"); 
+
+# add a column in tq with seconds
+tq$Seconds = time$hour*3600 + time$min*60 + time$sec;
+tq = tq[tq$Seconds > (8*3600 + 5*60) & tq$Seconds < (16*3600 + 25*60),]
+# tq: 780106 obs. of 10 variables
+
+# add Absolute Spread
+tq$Abs.Spread = tq$Ask.Price-tq$Bid.Price
+
+# add Mid Price and Locf
+tq$Mid.Price = (tq$Ask.Price + tq$Bid.Price) / 2
+tq$Mid.Price = na.locf(tq$Mid.Price);
+
+# get the direction of the trade
+tq$D = sign(tq$Price - tq$Mid.Price)
+
+# order flow
+tq$Q = tq$Volume * tq$D
+
+# add relative spread s
+tq$s = tq$Abs.Spread / tq$Mid.Price
+
+# add effective spread se
+tq$se = abs(tq$Price - tq$Mid.Price) / tq$Mid.Price
+
+save(tq, file ='tqAssigment2')
+# tq: 780106 obs. of 16 variables
