03d3adf2251d2a92acbbc620ccd791097703ec50
Mario Loera <marioll@kth.se>
2020-10-13 18:01:26 +0200

    rename files

--- SRC/assignment_2.2_realized_spread.R
+++ SRC/assignment_2.2_realized_spread.R
@@ -45,3 +45,55 @@ tt_Lead = lagPrices(trades = tt, quotes = tq, delay = 60 * 1, variable = "Mid.Pr
 # realize spread after one minute
 tt$sr = tt$se - abs(tt_Lead$Delay.Mid.Price - tt_Lead$Price)/tt_Lead$Price
 # 198893 obs, of 9 variables
+
+
+# get traiding days in a vector
+trading.days = levels(factor(tq$Date))
+
+# define a data frame report
+report <- data.frame(
+  day = trading.days,
+  obs = NaN,
+  vol.sek = NaN, # trade volueme sek
+  s.bsp = NaN, # quoted spread bsp 
+  w_s.bsp = NaN, #  weighted quoted spread bsp
+  se.bsp = NaN, #  effective spread bsp
+  w_se.bsp = NaN, # weighted effective spread bsp
+  ilr.bsp = NaN # illiquidity ratio bsp
+)
+
+# calculate meas for each traiding day
+for(day in trading.days){
+  
+  # save data for one day in td
+  td = tq[tq$Date == day, ]
+  
+  # transform day to index
+  i = which(trading.days %in% day)
+  
+  # observation per day
+  report$obs[i] = count(td$Date)[[2]]
+  
+  # b. The total trading volume, reported in million SEK
+  report$vol.sek[i] = sum((td$Volume * td$Price), na.rm = 'TRUE')
+  
+  # a. The quoted spread, reported in basis points
+  report$s.bsp[i] = 10000 * mean(td$s, na.rm = 'TRUE') 
+  
+  # a.* weighted quoted spread
+  report$w_s.bsp[i] =
+    10000 * weighted.mean(td$s, c(diff(td$Seconds), 0) , na.rm = T)
+
+  # c. The effective spread, reported in basis points
+  report$se.bsp[i] = 10000 * mean(td$se, na.rm = 'TRUE')
+  
+  # c.* weighted effective spread
+  report$w_se.bsp[i] = 10000 * weighted.mean(td$se, td$Volume, na.rm = 'TRUE')
+  
+  # d. The illiquidity ratio by Amihud (2002), 
+  # reported in percent per million SEK
+  # TODO
+  # report$ilr.bsp[i] =
+}
+
+report
