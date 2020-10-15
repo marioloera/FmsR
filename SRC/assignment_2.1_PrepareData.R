bd3ffbb0053649356aeafeabdb448d846c05ad8e
Mario Loera <marioll@kth.se>
2020-10-15 17:13:48 +0200

    apply outliers filetering

--- SRC/assignment_2.1_PrepareData.R
+++ SRC/assignment_2.1_PrepareData.R
@@ -4,6 +4,7 @@ setwd("C:/Users/MarioLoeraLozano/Dropbox/R/Data")
 # packages needed
 library(plyr) # install.packages('plyr')
 library(zoo) # install.packages('zoo')
+library(lattice); # install.packages('lattice')
 
 # clean workspace and variables
 rm(list = setdiff(ls(), lsf.str()))
@@ -11,6 +12,44 @@ rm(list = setdiff(ls(), lsf.str()))
 # loading data
 load("SKFb_Mar2020")
 # tq: 810224 obs. of 9 variables
+
+outlierKD <- function(dt, var) {
+  var_name <- eval(substitute(var),eval(dt))
+  na1 <- sum(is.na(var_name))
+  m1 <- mean(var_name, na.rm = T)
+  par(mfrow=c(2, 2), oma=c(0,0,3,0))
+  boxplot(var_name, main="With outliers")
+  hist(var_name, main="With outliers", xlab=NA, ylab=NA)
+  outlier <- boxplot.stats(var_name)$out
+  mo <- mean(outlier)
+  var_name <- ifelse(var_name %in% outlier, NA, var_name)
+  boxplot(var_name, main="Without outliers")
+  hist(var_name, main="Without outliers", xlab=NA, ylab=NA)
+  title("Outlier Check", outer=TRUE)
+  na2 <- sum(is.na(var_name))
+  cat("\nOutliers identified:", na2 - na1, "n")
+  cat("\nPropotion (%) of outliers:", round((na2 - na1) / sum(!is.na(var_name))*100, 1), "n")
+  cat("\nMean of the outliers:", round(mo, 2), "n")
+  m2 <- mean(var_name, na.rm = T)
+  cat("\nMean without removing outliers:", round(m1, 2), "n")
+  cat("\nMean if we remove outliers:", round(m2, 2), "n")
+  response <- readline(prompt="Do you want to remove outliers and to replace with NA? [yes/no]: ")
+  if(response == "y" | response == "yes"){
+    dt[as.character(substitute(var))] <- invisible(var_name)
+    assign(as.character(as.list(match.call())$dt), dt, envir = .GlobalEnv)
+    cat("\nOutliers successfully removed", "n")
+    return(invisible(dt))
+  } else{
+    cat("\nNothing changed", "n")
+    return(invisible(var_name))
+  }
+}
+
+par(mar=c(1,1,1,1)) 
+outlierKD(tq,Price)
+y
+outlierKD(tq,Volume)
+y
 
 # create a variable for time
 time = strptime(tq$Time, format="%H:%M:%OS"); 
