7eee175f49a9bbaacdd1783e66654eaa4360823e
Mario Loera <marioll@kth.se>
2020-10-16 04:12:05 +0200

    zadka per week estimate effective spread

--- SRC/assignment_2.5_zadka_per_week.R
+++ SRC/assignment_2.5_zadka_per_week.R
@@ -31,16 +31,23 @@ tt_all_data: 198893 obs. of 10 variables
 "
 tt_all_data = tt
 
+"
+ Use your coefficient estimates to obtain 
+  the estimated effective spread, 
+  the estimated adverse selection costs, 
+  and the estimated order processing and inventory costs.
+  
+  Report your estimates in basis points relative to the midpoint
+"
 # data frame for weeks data
 report_per_week <- data.frame(
   order = NaN,
   week = weeks,
   obs = NaN,
-  zadka_A = NaN,
-  zadka_B = NaN
+  asc_ = NaN, # " the estimated adverse selection costs, asc_ =  Lamda0*psi + Lamda1*eta "
+  opc_ic_ = NaN, # " and the estimated order processing and inventory costs. opc_ic_ = gamma0*D + gama1*Q "
+  se_ = NaN #  " the estimated effective spread   se_ = asc_ + opc_ic_ "
 )
-
-
 
 # calculate meas for each traiding day
 for(week in weeks){
@@ -63,29 +70,35 @@ for(week in weeks){
   #psi represents unexpected direction of trade
   tt$psi = tt$D - (1 - 2 * pnorm(- (tt$Q - tt$eta) / sd(tt$eta, na.rm=T)));
   
+  # use SadkaA (nominal price differences)
   sadkaA = lm(dP ~ psi + eta + dD + dQ, data = tt)
-
-  # zadkaB do the same model using returns instead of price change
-  tt$R = c(NA, diff(log(tt$Price)))
   
-  sadkaB = lm(R ~ psi + eta + dD + dQ, data = tt)
+  #get the coefficients
+  lamda0 = sadkaA[["coefficients"]][c('psi')]
+  lamda1 = sadkaA[["coefficients"]][c('eta')]
+  gamma0 = sadkaA[["coefficients"]][c('dD')]
+  gamma1 = sadkaA[["coefficients"]][c('dQ')]
   
-  report_per_week$zadka_A[i] = sadkaA
-  report_per_week$zadka_B[i] = sadkaB
+  # asc_ =  Lamda0*psi + Lamda1*eta
+  tt$asc_ = lamda0 * tt$psi + lamda1 * tt$eta
   
+  # opc_ic_ = gamma0*D + gama1*Q
+  tt$opc_ic_ = gamma0 * tt$D + gamma1 * tt$Q
+  
+  # se_ = d*(asc_ + opc_ic_) *** relative to the midpoint
+  tt$se_ = tt$D * (tt$asc_ + tt$opc_ic_)
+  
+  # get the average per week
+  report_per_week$lamda0[i] =  lamda0
+  report_per_week$lamda1[i] =  lamda1
+  report_per_week$gamma0[i] =  gamma0
+  report_per_week$gamma1[i] =  gamma1
+  report_per_week$asc_[i] =  mean(tt$asc_, na.rm = T)
+  report_per_week$opc_ic_[i] =  mean(tt$opc_ic_, na.rm = T)
+  report_per_week$se_[i] =  mean(tt$se_, na.rm = T)
 
-
+  #report_per_week$zadka_A[i] = sadkaA
+  #report_per_week$zadka_B[i] = sadkaB
 }
+
 report_per_week
-
-summary(report_per_week$zadka_A[[1]])
-
-
-# zadka_A[1] first week
-report_per_week$zadka_A[[1]]
-
-# zadka_A[1] first week, columns 2:4 (psi, eta, dD)
-report_per_week$zadka_A[[1]][2:4]
-
-report_per_week$zadka_A[[1]][c('psi')]
-
